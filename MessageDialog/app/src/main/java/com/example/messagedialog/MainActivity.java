75ff9474b3653de901760442039c8d0d3a325c35
FirManz <muchammadadifirmansyah@gmail.com>
2024-08-05 15:56:38 +0700

    vidio 14

--- MessageDialog/app/src/main/java/com/example/messagedialog/MainActivity.java
+++ MessageDialog/app/src/main/java/com/example/messagedialog/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.messagedialog;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,41 +20,53 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        System.out.println("onCreate");
+
     }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-
-        System.out.println("onStart");
-        }
-
-    @Override
-    protected void onPostResume(){
-        super.onPostResume();
-
-        System.out.println("onPostResume");
-        }
-
-    @Override
-    protected  void onPause() {
-        super.onPause();
-
-        System.out.println("onPause");
-        }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-
-        System.out.println("onStop");
+    public void showToast(String pesan){
+        Toast.makeText(this, pesan, Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
+    public void showAlert(String pesan) {
+        AlertDialog.Builder buatAlert = new AlertDialog.Builder(this);
+        buatAlert.setTitle("PERHATIAN !");
+        buatAlert.setMessage(pesan);
 
-        System.out.println("onDestroy");
+        buatAlert.show();
+    }
+
+    public void showAlertButton(String pesan) {
+        AlertDialog.Builder showAlert = new AlertDialog.Builder(this);
+        showAlert.setTitle("PERINGATAN !");
+        showAlert.setMessage(pesan);
+
+        showAlert.setPositiveButton("YA", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                showToast("Data Sudah Dihapus !");
+
+          }
+        });
+
+        showAlert.setNegativeButton("TIDAK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                showToast("Data Tidak Dihapus");
+            }
+        });
+
+        showAlert.show();
+        }
+
+    public void btnToast(View view) {
+        showToast("Selamat Belajar");
+    }
+
+    public void btnAlert(View view) {
+        showToast("Selamat Belajar");
+    }
+
+    public void btnAlertDialogButton(View view) {
+        showAlertButton("Yakin Akan Menghapus ?");
     }
 }
