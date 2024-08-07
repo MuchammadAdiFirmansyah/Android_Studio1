71b24bed7aa1b6b89c6aeacbeafcd2f1ff97ab32
FirManz <muchammadadifirmansyah@gmail.com>
2024-08-07 21:35:36 +0700

    vidio 17

---
+++ DatePicker/app/src/main/java/com/example/datepicker/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.datepicker;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etTanggal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        load();
+    }
+    public void load() {
+        etTanggal = findViewById(R.id.etTanggal);
+    }
+
+    public void etTanggal(View view) {
+
+        Calendar cal = Calendar.getInstance();
+        int tgl = cal.get(Calendar.DAY_OF_MONTH);
+        int bln = cal.get(Calendar.MONTH);
+        int thn = cal.get(Calendar.YEAR);
+
+        DatePickerDialog dtp = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                etTanggal.setText(tgl + "-" + "-" + thn);
+            }
+        }, tgl, bln, thn);
+
+       dtp.show();
+    }
+}
